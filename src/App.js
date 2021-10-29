747e842f3b1109c401d8b3b9c35073f93c7c1856
bannadas <bannadaso39@gmail.com>
2021-10-29 20:51:33 +0600

    git location chngd

--- src/App.js
+++ src/App.js
@@ -1,47 +1,10 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import logo from './logo.svg';
 import './App.css';
-import About from "./components/About/About";
-
-
-import Home from './components/Home/Home';
-import MyOders from "./components/MyOrders/MyOders";
-import SignIn from "./components/SignIn/SignIn";
-import AuthProvider from "./context/AuthProvider";
-import PrivateRoute from "./privateRoute/PrivateRoute";
 
 function App() {
   return (
-    <div>
-      <AuthProvider>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route exact path="/home">
-            <Home></Home>
-          </Route>
-         
-
-          <Route path="/login">
-          <SignIn></SignIn>
-          </Route>
-          <PrivateRoute path="/about">
-            <About></About>
-          </PrivateRoute>
-          
-          <PrivateRoute path="/myorders">
-        <MyOders></MyOders>
-          </PrivateRoute>
-
-        </Switch>
-      </Router>
-      </AuthProvider>
+    <div className="App">
+      
     </div>
   );
 }
--- src/Firebase/firebase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBdnWfiMwRRx8rti2NcaD90DAxWmN4sioo",
-    authDomain: "food-delivery-service-a28bb.firebaseapp.com",
-    projectId: "food-delivery-service-a28bb",
-    storageBucket: "food-delivery-service-a28bb.appspot.com",
-    messagingSenderId: "965690887667",
-    appId: "1:965690887667:web:a1baf3cdcf58e2a8f2d686"
-  };
-  export default firebaseConfig;
