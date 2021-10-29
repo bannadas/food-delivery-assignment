296826cdc426bd2d67a4da0820391aefdd502954
bannadas <bannadaso39@gmail.com>
2021-10-29 23:29:18 +0600

    add an item form

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import NavigationBar from '../NavigationBar/NavigationBar';
 
 const AddItem = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+     
+      
     return (
         <div>
-            <h1>Add an item</h1>
+
+            <NavigationBar></NavigationBar>
+      <h1 className="mt-5 text-center text-info">Add An Item</h1>
+      <div className="login-box w-25 m-auto mt-5">
+        <div className="event-box border border d-flex justify-content-center align-items-center">
+          <div className="login-form">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                {...register("name")}
+                placeholder="name"
+                className="p-2 m-2 w-100"
+              />
+              <br />
+             
+              <input
+                {...register("description")}
+                placeholder="Description"
+                className="p-2 m-2"
+                className="p-2 m-2 w-100"
+              />
+              <br />
+
+              <input
+                {...register("image", { required: true })}
+                placeholder="Image Link"
+                className="p-2 m-2"
+                className="p-2 m-2 w-100"
+              />
+              <br />
+
+
+              {errors.exampleRequired && <span>This field is required</span>}
+
+              <input type="submit" value="Add" className="btn btn-info w-50" />
+            </form>
+          </div>
         </div>
+      </div>
+    </div>
     );
 };
+
 
 export default AddItem;
