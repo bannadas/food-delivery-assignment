cedc5b49806ee5a23a119aa1fc5037cbcc0f87a0
bannadas <bannadaso39@gmail.com>
2021-10-30 19:56:07 +0600

    deletation added

--- src/components/Home/FoodItem/FoodItem.js
+++ src/components/Home/FoodItem/FoodItem.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './FoodItem.css'
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import './FoodItem.css'
 
-const FoodItem = ({item}) => {
-    const {_id,name,description,image} = item;
-    return (
+// const FoodItem = ({item}) => {
+//     const {_id,name,description,image} = item;
+//     return (
        
-            <div className=" card pb-3">
-            <img src={image} alt="" />
-            <h3>{name}</h3>
+//         //     <div className=" card pb-3">
+//         //     <img src={image} alt="" />
+//         //     <h3>{name}</h3>
            
-            <p className="px-3">{description}</p>
-            <Link to={`/item/${_id}`}>
-                <button className="btn btn-warning">order {name.toLowerCase()}</button>
-            </Link>
-        </div>
+//         //     <p className="px-3">{description}</p>
+//         //     <Link to={`/item/${_id}`}>
+//         //         <button className="btn btn-warning">order {name.toLowerCase()}</button>
+//         //     </Link>
+//         // </div>
        
-    );
-};
+//     );
+// };
 
-export default FoodItem;
+// export default FoodItem;
--- src/components/Home/FoodItems/FoodItems.js
+++ src/components/Home/FoodItems/FoodItems.js
@@ -1,20 +1,53 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import FoodItem from '../FoodItem/FoodItem';
 import './FoodItems.css'
 
 const FoodItems = () => {
     const [items,setItems] = useState([]);
+    const [isDeleted,setIsdeleted] = useState(null);
     useEffect(()=>{
         fetch('http://localhost:5000/items')
         .then(res => res.json())
         .then(data => setItems(data))
-    },[])
+    },[isDeleted])
+
+
+    const handleDelete = (id) =>{
+        fetch(`http://localhost:5000/deleteItem/${id}`,{
+            method:"DELETE",
+            headers:{'content-type':'application/json'},
+        })
+        .then(res => res.json())
+        .then(result => {
+            if(result.deleteCount){
+                setIsdeleted(true);
+            }
+            else{
+                setIsdeleted(false);
+            }
+        });
+            console.log(id);
+    }
     return (
         <div id="food-items">
             <h2 className="text-primary mt-5">All items</h2>
             <div className=" container food-container">
                {
-                items.map(item => <FoodItem key={item.id} item={item}></FoodItem>)
+                items.map(item => <div className=" card pb-3">
+                <img src={item.image} alt="" />
+                <h3>{item.name}</h3>
+               
+                <p className="px-3">{item.description}</p>
+
+
+
+                <button onClick={()=>handleDelete(item._id)} className="btn btn-danger">Delete</button>
+
+
+                    <button className="btn btn-warning">order {item.name.toLowerCase()}</button>
+               
+            </div>)
                 }
             </div>
         </div>
